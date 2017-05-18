fcf708c6d0a86b00474d04800cf3ed6490569848
Edward McCarthy <efmccarthy26@gmail.com>
2017-05-17 22:47:41 -0600

    fixed the mongo build by deleting previous mongo installations. I believe the PATH was set incorrect; I may have used homebrew to install and link up apps in the past, then tried new workflows

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -2,10 +2,17 @@ const MongoClient = require("mongodb").MongoClient;
 
 MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
   if (err) {
-    console.log("Can't connect to MongoDB server...")
+    return console.log("Can't connect to MongoDB server...")
   }
   console.log("Connected to MongoDB server")
 
-  
+  // db.collection("Todos").insertOne({
+  //   text: "Something to do",
+  //   completed: false
+  // }, (err, result));
+
   db.close();
 });
+
+// go here ~/mongo/bin in terminal
+// ./mongod --dbpath ~/mongo-data

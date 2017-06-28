7d780c255b68eb58c73c577b9fd90d373af037ae
Edward McCarthy <efmccarthy26@gmail.com>
2017-06-28 17:51:23 -0600

    added 401 test after fixing mongo connection; that took longer than expected

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -31,4 +31,20 @@ describe("POST /todos", () => {
         }).catch((e) => done(e));
       })
   })
+
+  it("shoudl not create todo with invalid body data", (done) => {
+    request(app)
+    .post("/todos")
+    .send({})
+    .expect(400)
+    .end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+      Todo.find().then((todos) => {
+        expect(todos.length).toBe(0);
+        done();
+      }).catch((e) => done(e));
+    });
+  });
 })

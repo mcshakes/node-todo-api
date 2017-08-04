3dcbc37bea1e08ed494f6a600ea33c0be64e36ac
Edward McCarthy <efmccarthy26@gmail.com>
2017-08-04 16:16:32 -0600

    Added a connector for mongoose connect; either the DB_URI set by Heroku or localhost

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp")
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp")
+// NOTE: MONGODB_URI set as Heroku Config. View in Terminal with `heroku config`
 module.exports = mongoose;

492adecd6518ee2d6d2d51160e7a0128f3d25353
Edward McCarthy <efmccarthy26@gmail.com>
2017-08-07 22:46:22 -0600

    settles on finding by ID and finished up Delete route

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -3,3 +3,15 @@ const {ObjectID} = require("mongodb");
 const {mongoose} = require("./../server/db/mongoose")
 const {Todo} = require("./../server/models/todo")
 const {User} = require("./../server/models/todo")
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+// NOTE: Running above shows what was found and removed up top at result: { n: 3, ok: 1 }
+
+// Todo.findOneAndRemove()
+
+// Todo.findByIdAndRemove
+Todo.findByIdAndRemove("59893c6cfb820973fc32b30b").then((todo) => {
+  console.log(todo);
+})

da6bf72b3fe74184946e15df1fe65d3ece016747
Edward McCarthy <efmccarthy26@gmail.com>
2017-08-08 07:50:11 -0600

    added Lodash and changed to consts

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var {ObjectID} = require("mongodb");
+const _ = require("lodash");
+const express = require("express");
+const bodyParser = require("body-parser");
+const {ObjectID} = require("mongodb");
 
-var {mongoose} = require("./db/mongoose")
-var {Todo} = require ("./models/todo")
-var {User} = require ("./models/user")
+const {mongoose} = require("./db/mongoose")
+const {Todo} = require ("./models/todo")
+const {User} = require ("./models/user")
 
 var app = express();
 const port = process.env.PORT || 3000;
